e148e8f571bdfba64f4b8c5fc948dd5fe202e182
chouchenglai <chouchenglai1981@gmail.com>
2026-05-02 03:10:17 +0800

    Add files via upload

--- soccer_app .py
+++ soccer_app .py
@@ -100,7 +100,7 @@ if os.path.exists(img_path):
                 text-align: center;
                 background-color: #ffffff;
                 padding: 0px 0;
-                margin-bottom: -50px;
+                margin-bottom: 50px;
                 border-radius: 12px;
             }}
             .banner-img {{
