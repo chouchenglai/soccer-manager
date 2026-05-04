6f4486f79510ab69b91f590568204a25db6606b2
chouchenglai <chouchenglai1981@gmail.com>
2026-05-04 10:32:57 +0800

    Add files via upload

--- soccer_app.py
+++ soccer_app.py
@@ -108,8 +108,8 @@ if main_df.empty:
         save_data(pd.DataFrame([row])); st.rerun()
 else:
     # 核心：標籤頁定義
-    tab1, tab2, tab_live, tab3, tab4, tab5 = st.tabs(["💰 下單投注", "📝 註冊帳號", "⚽ 即時比分", "📋 歷史記錄", "📊 統計圖表",  "💬 討 論 區"])
-
+    tab1, tab2, tab_live, tab3, tab4, tab5 = st.tabs(["💰 下單投注", "**📝 註冊帳號**", "⚽ 即時比分", "📋 歷史記錄", "📊 統計圖表",  "💬 討 論 區"])
+       
     with tab1: # 下單投注
         try: balance = int(main_df["結算總分"].iloc[-1])
         except: balance = 0
@@ -223,6 +223,7 @@ else:
                     st.rerun()
 
     with tab2: # 註冊帳號
+                st.markdown("<h2 style='color:#1E90FF; font-weight:bold;'>📝 註冊帳號</h2>", unsafe_allow_html=True)
         st.subheader("📁 登錄會員管理中心")
         
         # --- 區塊 1：新增帳號 ---
